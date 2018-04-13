dd7c410e03f8308d4a217757599175bcd7f67ac8
egodasa <egodasa@gmail.com>
2018-04-14 00:25:44 +0700

    Ubah tema login

--- resources/assets/js/views/login.vue
+++ resources/assets/js/views/login.vue
@@ -1,32 +1,28 @@
 <template>
-<content-section>
-<template slot="contentHeader">
-	<div class="login-box">
-	  <div class="login-logo">
-	    <a href="">HOTEL <b>MEVIT</b></a>
-	  </div>
-	  <!-- /.login-logo -->
-	  <div class="login-box-body" id="app">
-	    <p class="login-box-msg">Login untuk masuk ke aplikasi</p>
-	
-	    <form @submit.prevent="checkLogin">
-	      <div class="form-group has-feedback">
-	        <input type="text" class="form-control" placeholder="Username" v-model="login.username">
-	        <span class="glyphicon glyphicon-user form-control-feedback"></span>
-	      </div>
-	      <div class="form-group has-feedback">
-	        <input type="password" class="form-control" placeholder="Password" v-model="login.password">
-	        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-	      </div>
-	      <div class="form-group has-feedback">
-	        <button type="submit" class="btn btn-primary btn-block btn-flat" :disabled="btnLogin.disabled">{{btnLogin.text}}</button>
-	      </div>
-	      
-	    </form>
-	  </div>
-	</div>
-</template>
-</content-section>
+<div class="login-box">
+  <div class="login-logo">
+    <a href="">HOTEL <b>MEVIT</b></a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="login-box-body" id="app">
+    <p class="login-box-msg">Login untuk masuk ke aplikasi</p>
+
+    <form @submit.prevent="checkLogin">
+      <div class="form-group has-feedback">
+        <input type="text" class="form-control" placeholder="Username" v-model="login.username">
+        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" class="form-control" placeholder="Password" v-model="login.password">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <button type="submit" class="btn btn-primary btn-block btn-flat" :disabled="btnLogin.disabled">{{btnLogin.text}}</button>
+      </div>
+      
+    </form>
+  </div>
+</div>
 </template>
 <script>
 export default{
@@ -42,6 +38,12 @@ export default{
 				disabled : false
 			}
 		}
+	},
+	beforeCreate (){
+		document.body.className = "hold-transition login-page";
+	},
+	destroyed (){
+		document.body.className = "skin-blue sidebar-mini";
 	},
 	methods : {
 		checkLogin (){
