b8ff916e73a3e927bdb2cc6d213b81b2370ecdad
egodasa <egodasa@gmail.com>
2018-04-15 19:58:57 +0700

    Deploy ke heroku, database postgresql

--- config/database.php
+++ config/database.php
@@ -13,7 +13,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => 'pgsql',
+    //~ 'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -57,10 +58,10 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'host'     => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-		    'database' => env('DB_DATABASE', 'forge'),
-		    'username' => env('DB_USERNAME', 'forge'),
-		    'password' => env('DB_PASSWORD', ''),
+            'port' =>'5432',
+		    'database' => 'dbmevit',
+		    'username' => 'postgres',
+		    'password' => 'qwe123*iop',
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
